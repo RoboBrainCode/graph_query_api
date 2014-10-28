1c49ff6bca5843e8f95511c6ad38cd1c4affb1af
ashesh <asheshjain399@gmail.com>
2014-10-27 21:01:11 -0700

    adding

--- nlquery/urls.py
+++ nlquery/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
-from nlquery import views
+from graph_query_api.nlquery import views
 
 urlpatterns = patterns('',
     url(r'langquery/', views.return_nlquery_result, name='nlquery'),
