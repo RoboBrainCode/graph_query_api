f418ff8b775698914d82ea51e292eb873a9673c1
ashesh <asheshjain399@gmail.com>
2014-10-27 20:36:11 -0700

    updating graph query

--- nlquery/views.py
+++ nlquery/views.py
@@ -1,1 +1,12 @@
 # Create your views here.
+from django.http import HttpResponse
+import json
+import numpy as np
+import query
+
+def return_nlquery_result(request):
+    query_in = request.GET.get('user_in','')
+    query_result = query.answerQuestion(query_in)
+
+    json_result = [{'user_out':query_result}]
+    return HttpResponse(json.dumps(json_result), content_type="application/json")
