10c440977969719cef729929ef218847d72474fa
ashesh <asheshjain399@gmail.com>
2014-10-30 17:19:10 -0700

    making graph_query_api a Django application

--- nlquery/urls.py
+++ nlquery/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url
-from graph_query_api.nlquery import views
+from nlquery import views
 
 urlpatterns = patterns('',
     url(r'nlquery/', views.return_nlquery_result, name='nlquery'),
--- urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-from graph_query_api import views
-
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'Backend.views.home', name='home'),
-    #url(r'nlquery/',include('graph_query_api.nlquery.urls')),
-)
