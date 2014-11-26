766f484836266a8eaf42962f64097b545b36f1a1
ashesh <asheshjain399@gmail.com>
2014-11-26 13:26:20 -0800

    adding graph

--- vizquery/views.py
+++ vizquery/views.py
@@ -1,24 +1,17 @@
-from django.shortcuts import render
-import sys
-import re
-import time
-from pprint import pprint
+from django.http import HttpResponse
 from neo4jrestclient.client import GraphDatabase
-from py2neo import cypher
-from lxml import html
-import requests
-import re
 import json
-import threading
+
 # Create your views here.
-def get_node(handle):
+def get_node(request):
+    handle = request.GET.get('handle','shoe')
+
     gdb = GraphDatabase("http://ec2-54-187-76-157.us-west-2.compute.amazonaws.com:7474/db/data/")
     url = "http://d1rygkc2z32bg1.cloudfront.net/"
     #q = "MATCH path = (n { handle: '"+handle+"' })-[r*1..3]-(x) RETURN r, n,x, path"
     q = "MATCH (n{handle:'"+handle+"'})-[r]-(x) RETURN r, n,x"
     #q = "MATCH (n{handle:'"+handle+"'})-[r]-(g)-[a]-(x) RETURN r, n,x,g,a"
     result = gdb.query(q=q)
-    #pprint(vars(result))
     length = len(result)
     nodes = {}
     links = {}
