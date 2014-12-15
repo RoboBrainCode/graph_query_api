04b99766665a075206da0af458600cd197922c84
asheshjain399 <asheshjain399@gmail.com>
2014-12-15 21:40:49 +0000

    adding

--- vizquery/views.py
+++ vizquery/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from neo4jrestclient.client import GraphDatabase
 import json
 import gc
+import requests
 
 # Create your views here.
 def get_node(request):
@@ -12,38 +13,44 @@ def get_node(request):
     #q = "MATCH path = (n { handle: '"+handle+"' })-[r*1..3]-(x) RETURN r, n,x, path"
     q = "MATCH (n{handle:'"+handle+"'})-[r]-(x) RETURN r, n,x"
     #q = "MATCH (n{handle:'"+handle+"'})-[r]-(g)-[a]-(x) RETURN r, n,x,g,a"
-    result = gdb.query(q=q)
+    #result = gdb.query(q=q)
+    result = requests.post(gdb._transaction + '/commit',
+            data=json.dumps({'statements': [{
+                'statement': "MATCH (n{handle:'"+handle+"'})-[r]-(x) RETURN r,n,x"}]
+                }),
+            headers={
+                'Accept': 'application/json; charset=UTF-8',
+                'Content-Type': 'application/json'
+                }).json();
+    result = result['results'][0]        
+    result = result['data']                
     length = len(result)
     nodes = {}
     links = {}
     id = 0
     for x in range(0,length):
-        relationship = result[x][0]
+        tuple = result[x]['row']
+        start_node = tuple[1]
+        end_node = tuple[2]
         #for relationship in relationships:
-        rel_self = relationship["self"]
+        rel_self = "link-"+start_node["handle"]+"-"+end_node["handle"]
         if rel_self not in links:
-            metadata = relationship["metadata"]
-            type = relationship["metadata"]["type"]
-            start = relationship["start"]
-            end = relationship["end"]
-            links[rel_self] = {"type":type,"start":start,"end":end}
-
-        start_node = result[x][1]
-        print start_node
-        start_node_self = start_node["self"]
-        if start_node_self not in nodes:
-            if "mediapath" in start_node["data"]:
-                nodes[start_node_self] = {"url":start_node_self, "handle":start_node["data"]["handle"], "mediapath": start_node["data"]["mediapath"]}
+            links[rel_self] = {"type":"BLANK","start":start_node['handle'],"end":end_node['handle']}                                                                                                    
+            
+        if start_node['handle'] not in nodes:       
+            if "mediapath" in start_node:   
+                nodes[start_node['handle']] = {"url":"a","handle":start_node["handle"], "mediapath": start_node["mediapath"]}
             else:
-                nodes[start_node_self] = {"url":start_node_self, "handle":start_node["data"]["handle"]}
-
-        end_node = result[x][2]
-        end_node_self = end_node["self"]
-        if end_node_self not in nodes:
-            if "mediapath" in end_node["data"]:
-                nodes[end_node_self] = {"url":end_node_self, "handle":end_node["data"]["handle"], "mediapath": url+end_node["data"]["mediapath"]}
+                nodes[start_node['handle']] = {"url":"a","handle":start_node["handle"]}
+        end_node_self = end_node["handle"]
+        if end_node_self not in nodes:  
+            if "mediapath" in end_node:     
+                nodes[end_node_self] = {"url":"a","handle":end_node["handle"], "mediapath": url+end_node["mediapath"]}                                                                                                                                                                          
             else:
-                nodes[end_node_self] = {"url":end_node_self, "handle":end_node["data"]["handle"]}
+                nodes[end_node_self] = {"url":"a","handle":end_node["handle"]}                                                                                                                                                                                                                                                                    
+                                                                                                                                                                                                                                                                                        
+
     json_result = {"nodes":nodes,"links":links}
+    del result
     gc.collect()
     return HttpResponse(json.dumps(json_result), content_type="application/json")
